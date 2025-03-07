920d45f5c7cdba2ad19ef3987fe9794965c62cf2
abhay <abhay.singh@tothenew.comgit config --global user.name abhaygit config --global user.email abhay.singh@tothenew.com>
2025-03-07 15:57:28 +0530

    add: landing page and navigation

---
+++ app/components/Header/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+import Image from "next/image";
+import styles from "../../styles/landing-page.module.css";
+import { useRouter } from "next/navigation";
+
+export default function Header() {
+    const router = useRouter();
+  return (
+    <div className={styles.container}>
+      {/* Navbar */}
+      <nav className={styles.navbar}>
+        <div className={styles.logo}>
+          {/* TODO: ADD IN MODULE CSS */}
+          <div className={styles.nav_container}>
+            <Image
+              src="/assets/mecare_logo.png"
+              alt="Logo"
+              width={40}
+              height={40}
+              onClick={() => router.push("/")}
+              style={{
+                cursor: "pointer",
+              }}
+            />
+
+            <h1>MedCare</h1>
+          </div>
+          <ul className={styles.navLinks}>
+            <li onClick={() => router.push("/home")}>Home</li>
+            <li onClick={() => router.push("/appointment")}>Appointments</li>
+            <li onClick={() => router.push("/health-blog")}>Health Blog</li>
+            <li onClick={() => router.push("/reviews")}>Reviews</li>
+          </ul>
+        </div>
+        <div className={styles.authButtons}>
+          <button className={styles.login}>Login</button>
+          <button className={styles.register}>Register</button>
+        </div>
+      </nav>
+    </div>
+  );
+}
---
+++ app/health-blog/page.tsx
@@ -0,0 +1,3 @@
+export default function HealthBlog(){
+    return <div>Health Blog</div>
+}
